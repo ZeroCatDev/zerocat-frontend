64df75ac5a6671592335b312f84cd038a7980f04
SunWuyuan <1847261658@qq.com>
2024-08-07 21:47:21 +0800

    1

--- src/components/ProjectRunner.vue
+++ src/components/ProjectRunner.vue
@@ -1,57 +1,50 @@
 <template>
-    <div v-if='type === "scratch"'>
-        <div style="width: 100%; height: 100%;aspect-ratio: 4 / 3;"><iframe :src="embedurl" scrolling="no"
-                frameborder="0" style="width: 100%; height: 100%;"></iframe></div>
-    </div>
-    <div v-if='type === "python"'>
-        <div style="width: 100%; height: 100%;aspect-ratio: 4 / 3;"><iframe :src="pythonplayer" scrolling="no"
-                frameborder="0" style="width: 100%; height: 100%;"></iframe></div>
-    </div>
+  <div v-if='type === "scratch"'>
+    <div style="width: 100%; height: 100%;aspect-ratio: 4 / 3;"><iframe :src="embedurl" scrolling="no" frameborder="0"
+        style="width: 100%; height: 100%;"></iframe></div>
+  </div>
+  <div v-if='type === "python"'>
+    <div style="width: 100%; height: 100%;aspect-ratio: 4 / 3;"><iframe :src="pythonplayer" scrolling="no"
+        frameborder="0" style="width: 100%; height: 100%;"></iframe></div>
+  </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            embedurl: `/`,
-            watchedtype: '',
-            watchedid: '',
-            pythonplayer: '',
-        }
-    },
-
-
-    props: {
-        type: {
-            type: String,
-            default: 'text',
-        },
-        id: {
-            type: String,
-            default: '1',
-        },
-
-    },
-    created() {
-        if (this.type === 'scratch') {
-                this.embedurl = '/scratch/embed.html#' + this.id
-            }
-            if (this.type === 'python') {
-                this.pythonplayer = import.meta.env.VITE_APP_BASE_API + '/python/next?id=' + this.id
-            }
-    },
-    watch
-        : {
-        type: function (newType) {
-            if (newType === 'scratch') {
-                this.embedurl = '/scratch/embed.html#' + this.id
-            }
-            if (newType === 'python') {
-                this.pythonplayer = import.meta.env.VITE_APP_BASE_API + '/python/next?id=' + this.id
-            }
-        },
-        id: function (newType) {
-            this.watchedid = newType
-        },
+  data() {
+    return {
+      embedurl: `/`,
+      watchedtype: '',
+      watchedid: '',
+      pythonplayer: '',
     }
+  },
+
+
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    id: {
+      type: String,
+      default: '1',
+    },
+
+  },
+
+  watch: {
+    type: function (newType) {
+      this.watchedtype = newType
+      if (newType === 'scratch') {
+        this.embedurl = import.meta.env.VITE_APP_SCARATCH + '/embed.html#' + this.id
+      }
+      if (newType === 'python') {
+        this.pythonplayer = import.meta.env.VITE_APP_PYTHON + '/nextplay.html?id=' + this.id
+      }
+    },
+    id: function (newType) {
+      this.watchedid = newType
+    },
+  }
 }
 </script>
--- src/pages/projects/my.vue
+++ src/pages/projects/my.vue
@@ -1,31 +1,41 @@
-<template>
-  <v-container>
-    <v-text-field :label="'作品名 包含：' + search.title" v-model="search.title"></v-text-field>
-    <v-text-field :label="'作品描述 包含：' + search.description" v-model="search.description"></v-text-field>
-    <v-text-field :label="'作品内容 包含：' + search.src" v-model="search.src"></v-text-field>
-    <v-number-input control-variant="用户ID" :label="'用户ID 为：' + search.authorid" v-model="userinfo.userid"
-      disabled></v-number-input>
+<template> <v-container>
+    <v-row><v-col cols="6">
+        <v-text-field :label="'作品名 包含：' + search.title" v-model="search.title"></v-text-field></v-col>
+      <v-col cols="6"><v-text-field :label="'作品描述 包含：' + search.description"
+          v-model="search.description"></v-text-field></v-col>
+      <v-col cols="6">
+        <v-text-field :label="'作品内容 包含：' + search.src" v-model="search.src"></v-text-field></v-col>
+      <v-col cols="6">
+        <v-number-input control-variant="用户ID" :label="'用户ID 为：' + search.authorid" v-model="userinfo.userid"
+          disabled></v-number-input></v-col>
 
-    <v-select v-model="search.order" :items="search.orderitems" item-title="name" item-value="order" label="排序"
-      return-object></v-select>
+      <v-col cols="3"><v-select v-model="search.order" :items="orderitems" item-title="name" item-value="type"
+          label="排序"></v-select></v-col>
 
-    <v-select v-model="search.type" :items="search.typeitems" item-title="name" item-value="type" label="项目类型"
-      return-object></v-select>
-    <v-select v-model="search.state" :items="searchstates" item-title="state" item-value="abbr" label="项目状态"></v-select>
-    <v-btn @click="onPageChange(1);"> 搜索 </v-btn>
-    <v-btn @click="
-      search.order = { name: '观看量升序', type: 'view_up' }; search.type = { name: '所有', type: '' }; search.authorid = '';
-    search.src = ''; search.description = ''; search.title = ''; onPageChange(1);">重置</v-btn>
+      <v-col cols="3"><v-select v-model="search.type" :items="typeitems" item-title="name" item-value="type"
+          label="项目类型"></v-select></v-col>
+      <v-col cols="3"><v-select v-model="search.state" :items="searchstates" item-title="state" item-value="abbr"
+          label="项目状态"></v-select></v-col>
+      <v-col cols="3">
+        <v-btn @click="onPageChange()"> 搜索 </v-btn>
+        <v-btn @click="
+          search.order = 'view_up';
+        search.type = '';
+        search.authorid = '';
+        search.src = '';
+        search.description = '';
+        search.title = '';
+        search.state = '';
+        onPageChange();
+        ">重置</v-btn></v-col></v-row>
     <br />
     <br />
-
 
 
 
     <Projects :authorid='userinfo.userid' :title="search.title" :description="search.description" :src='search.src'
       :order="search.order.type" :type="search.type.type" ref="Projects" showinfo='true' :state="search.state"
       :actions="[{ name: '信息', function: openinfo }, { name: '编辑', function: openedit }]"></Projects>
-
     <v-dialog v-model="dialog" max-width="70vw" persistent origin='center center'>
 
 
@@ -33,22 +43,21 @@
         <v-card-text>
           <v-row dense>
             <v-col cols="12" md="12" sm="12">
-              <v-text-field label="标题" required v-model="nowProject.title"></v-text-field>
+              <v-text-field label="标题" required v-model="nowProject.title" hint="将会在首页展示"></v-text-field>
             </v-col>
 
             <v-col cols="12" md="12" sm="12">
-              <v-text-field hint="example of helper text only on focus" label="Middle name"
-                v-model="nowProject.description"></v-text-field>
+              <v-textarea hint="介绍作品类型，玩法，并向对这个作品有帮助的人致谢！" label="简介" v-model="nowProject.description"></v-textarea>
             </v-col>
 
 
 
 
             <v-col cols="12" sm="6">
-              <v-select :items="['scratch', 'python', 'text']" label="type" required
+              <v-select :items="['scratch', 'python', 'text']" label="类型" required hint="不建议你改"
                 v-model="nowProject.type"></v-select>
             </v-col> <v-col cols="12" sm="6"><v-select v-model="nowProject.state" :items="projectstates"
-                item-title="state" item-value="abbr" label="项目状态"></v-select> </v-col>
+                item-title="state" item-value="abbr" label="项目状态" hint="我们鼓励开源"></v-select> </v-col>
 
 
             <!--<v-col cols="12" sm="6">
@@ -56,11 +65,11 @@
                 :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
                 label="Interests" auto-select-first multiple v-model="nowProject.tags"></v-autocomplete>
             </v-col>-->
-        <v-expansion-panels> <v-expansion-panel> <v-expansion-panel-title>详细数据</v-expansion-panel-title>
-            <v-expansion-panel-text>
-              {{ nowProject }} </v-expansion-panel-text>
-          </v-expansion-panel>
-        </v-expansion-panels>
+            <v-expansion-panels> <v-expansion-panel> <v-expansion-panel-title>详细数据</v-expansion-panel-title>
+                <v-expansion-panel-text>
+                  {{ nowProject }} </v-expansion-panel-text>
+              </v-expansion-panel>
+            </v-expansion-panels>
           </v-row>
 
         </v-card-text>
@@ -77,12 +86,14 @@
     </v-dialog>
 
   </v-container>
+
+
 </template>
 
 <script>
 import request from "../../axios/axios";
 import Projects from "../../components/Projects.vue";
-import {localuser} from '@/stores/user';
+import { localuser } from '@/stores/user';
 export default {
   components: { Projects },
 
@@ -100,6 +111,19 @@ export default {
         { state: '开源', abbr: 1 },
         { state: '被精选', abbr: 2 },
       ],
+      typeitems: [
+        { name: "所有", type: "" },
+        { name: "Scratch", type: "scratch" },
+        { name: "Python", type: "python" },
+      ],
+      orderitems: [
+        { name: "观看量升序", type: "view_up" },
+        { name: "观看量降序", type: "view_down" },
+        { name: "时间升序", type: "time_up" },
+        { name: "时间降序", type: "time_down" },
+        { name: "序号升序", type: "id_up" },
+        { name: "序号降序", type: "id_down" },
+      ],
       nowProjectID: 0,
       dialog: false,
       nowProject: {},
@@ -109,22 +133,10 @@ export default {
         type: "",
         description: "",
         src: "",
-        order: { name: "观看量升序", type: "view_up" },
+        order: "view_up",
         authorid: "",
-        type: { name: "所有", type: "" },
-        typeitems: [
-          { name: "所有", type: "" },
-          { name: "Scratch", type: "scratch" },
-          { name: "Python", type: "python" },
-        ],
-        orderitems: [
-          { name: "观看量升序", type: "view_up" },
-          { name: "观看量降序", type: "view_down" },
-          { name: "时间升序", type: "time_up" },
-          { name: "时间降序", type: "time_down" },
-          { name: "序号升序", type: "id_up" },
-          { name: "序号降序", type: "id_down" },
-        ],
+        type: "",
+
         state: ''
       },
       userinfo: localuser.user,
