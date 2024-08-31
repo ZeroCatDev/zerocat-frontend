b9e3da41ed01efd01c7eb5e6d78eabd241761a6c
SunWuyuan <1847261658@qq.com>
2024-08-31 10:08:46 +0800

    1

--- src/main.js
+++ src/main.js
@@ -27,6 +27,9 @@ app.use(ToastService);
 // 使用 Vercel Speed Insights 观测网站性能
 import { injectSpeedInsights } from '@vercel/speed-insights';
 injectSpeedInsights();
+// 使用 Vercel Analytics 收集数据
+import { inject } from '@vercel/analytics';
+inject();
 
 registerPlugins(app);
 app.mount("#app");
