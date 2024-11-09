0f174c6ace75841b3eef0de11b43e3e06a2c1211
SunWuyuan <1847261658@qq.com>
2024-11-09 19:46:14 +0800

    1

--- src/pages/account/totp/index.vue
+++ src/pages/account/totp/index.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <v-btn @click="generateTOTP" color="primary" class="mb-4">生成新的TOTP验证器</v-btn>
+        <v-btn to="./totp/add" color="primary" class="mb-4">生成新的TOTP验证器</v-btn>
       </v-col>
       <v-col cols="12">
         <v-list>
@@ -16,9 +16,9 @@
               <v-list-item-subtitle>{{ totp.status }}</v-list-item-subtitle>
             </v-list-item-content>
             <v-list-item-action>
-              <v-btn @click.stop="renameTOTP(totp.id)" color="info">重命名</v-btn>
+              <v-btn @click.stop="renameTOTP(totp.id)" color="primary">重命名</v-btn>
               <v-btn @click.stop="deleteTOTP(totp.id)" color="error">删除</v-btn>
-              <v-btn @click.stop="activateTOTP(totp.id)" color="success">激活</v-btn>
+              <!--<v-btn @click.stop="activateTOTP(totp.id)" color="success">激活</v-btn>-->
             </v-list-item-action>
           </v-list-item>
         </v-list>
@@ -39,7 +39,7 @@ export default {
   methods: {
     async fetchTOTPList() {
       try {
-        const response = await request.get("/totp/list");
+        const response = await request.get("/account/totp/list");
         if (response.status === "success") {
           this.totpList = response.data.list;
         } else {
@@ -59,11 +59,11 @@ export default {
         });
       }
     },
-    async renameTOTP(totpId) {
+    async renameTOTP(totp_id) {
       const newName = prompt("请输入新的名称");
       if (newName) {
         try {
-          const response = await request.post("/totp/rename", { totpId, name: newName });
+          const response = await request.post("/account/totp/rename", { totp_id, name: newName });
           if (response.status === "success") {
             this.fetchTOTPList();
             this.$toast.add({
@@ -90,9 +90,9 @@ export default {
         }
       }
     },
-    async deleteTOTP(totpId) {
+    async deleteTOTP(totp_id) {
       try {
-        const response = await request.post("/totp/delete", { totpId });
+        const response = await request.post("/account/totp/delete", { totp_id });
         if (response.status === "success") {
           this.fetchTOTPList();
           this.$toast.add({
@@ -118,68 +118,9 @@ export default {
         });
       }
     },
-    async generateTOTP() {
-      try {
-        const response = await request.post("/totp/generate");
-        if (response.status === "success") {
-          this.$toast.add({
-            severity: "success",
-            summary: "成功",
-            detail: `新的TOTP验证器已生成: ${response.data.secret}`,
-            life: 3000,
-          });
-          this.fetchTOTPList();
-        } else {
-          this.$toast.add({
-            severity: "error",
-            summary: "错误",
-            detail: response.message,
-            life: 3000,
-          });
-        }
-      } catch (error) {
-        this.$toast.add({
-          severity: "error",
-          summary: "错误",
-          detail: error.message,
-          life: 3000,
-        });
-      }
-    },
-    async activateTOTP(totpId) {
-      const totpToken = prompt("请输入TOTP令牌");
-      if (totpToken) {
-        try {
-          const response = await request.post("/totp/activate", { totpId, totpToken });
-          if (response.status === "success") {
-            this.fetchTOTPList();
-            this.$toast.add({
-              severity: "success",
-              summary: "成功",
-              detail: "TOTP验证器激活成功",
-              life: 3000,
-            });
-          } else {
-            this.$toast.add({
-              severity: "error",
-              summary: "错误",
-              detail: response.message,
-              life: 3000,
-            });
-          }
-        } catch (error) {
-          this.$toast.add({
-            severity: "error",
-            summary: "错误",
-            detail: error.message,
-            life: 3000,
-          });
-        }
-      }
-    },
   },
   mounted() {
     this.fetchTOTPList();
-  },
+  }
 };
 </script>
