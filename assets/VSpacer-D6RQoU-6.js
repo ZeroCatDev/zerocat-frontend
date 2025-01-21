b5cec80fabb99553761cb08cb48f3eff1409572e
SunWuyuan <SunWuyuan@users.noreply.github.com>
2025-01-21 12:24:36 +0000

    deploy: 2247f313e0ec2276d9bbae9b4484523e44902be9

--- assets/VSpacer-D6RQoU-6.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{ak as a}from"./index-D4SP7Gxj.js";const p=a("v-spacer","div","VSpacer");export{p as V};
---
+++ assets/openEdit-AJJkPSPH.js
@@ -0,0 +1,1 @@
+function n(o,t){t=="scratch"&&window.open("/scratch/editor.html?id="+o),t=="python"&&window.open("/python/edit.html?id="+o),t=="text"&&window.open("/python/edit.html?id="+o)}export{n as o};
