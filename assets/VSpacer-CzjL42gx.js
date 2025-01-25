8ecad5858a80ef034ac27c7e8bbcc548b4060880
SunWuyuan <SunWuyuan@users.noreply.github.com>
2025-01-25 08:29:45 +0000

    deploy: e3bdd6d42328e4ff6f548bf94f8b7acfb8f2d46e

---
+++ assets/VSpacer-CzjL42gx.js
@@ -0,0 +1,1 @@
+/* empty css              */import{ar as r}from"./index-Bxp1Qw7I.js";const p=r("v-spacer","div","VSpacer");export{p as V};
---
+++ assets/default-B4XMFQCs.js
@@ -0,0 +1,1 @@
+import{r as o,c as r,w as e,V as n,o as s,a as t,b as _}from"./index-Bxp1Qw7I.js";const f={__name:"default",setup(c){return(l,u)=>{const a=o("router-view");return s(),r(n,null,{default:e(()=>[t(_,null,{default:e(()=>[t(a)]),_:1})]),_:1})}}};export{f as default};
