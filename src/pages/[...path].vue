160d538755ccede5983cd612184205895bdb8c4b
SunWuyuan <1847261658@qq.com>
2025-01-30 16:17:38 +0800

    添加路由重定向逻辑，处理以 '/proxy' 开头的路径

--- src/pages/[...path].vue
+++ src/pages/[...path].vue
@@ -3,6 +3,13 @@
 </template>
 
 <script setup>
-//
+import { useRouter, useRoute } from 'vue-router';
 import error404 from "@/components/error/404.vue";
+
+const router = useRouter();
+const route = useRoute();
+
+if (route.path.startsWith('/proxy')) {
+  router.replace(`/app${route.path}`);
+}
 </script>
