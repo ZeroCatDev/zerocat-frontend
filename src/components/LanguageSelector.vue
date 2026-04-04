c059ae6b7060b5c5b168b7f40d11dbbff803b018
Sunwuyuan <sun@wuyuan.dev>
2026-04-04 16:19:41 +0800

    feat: 重构语言选择器组件，优化类型展示和对话框逻辑

--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -1,76 +1,79 @@
 <template>
   <div>
-    <v-text-field
-      :label="label"
-      :model-value="modelValue"
-      :required="required"
-      append-inner-icon="mdi-menu-down"
-      readonly
-      variant="outlined"
-      @click="dialog = true"
-    ></v-text-field>
+    <div class="text-subtitle-2 mb-2 d-flex align-center ga-1">
+      <span>{{ label }}</span>
+      <span v-if="required" class="text-error">*</span>
+    </div>
 
-    <v-dialog v-model="dialog" >
+    <v-row>
+      <v-col
+        v-for="type in allTypes"
+        :key="type.key"
+        cols="12"
+        sm="6"
+        md="3"
+      >
+        <v-card
+          :color="modelValue === type.key ? 'primary' : undefined"
+          class="pa-4 cursor-pointer fill-height"
+          :variant="modelValue === type.key ? 'tonal' : undefined"
+          @click="selectLanguage(type.key)"
+          border
+          hover
+        >
+          <div class="d-flex align-center">
+            <v-icon :icon="type.icon || 'mdi-shape-outline'" class="mr-2"></v-icon>
+            <div>
+              <div class="text-subtitle-1">{{ type.name }}</div>
+              <div class="text-caption">{{ type.description || type.key }}</div>
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" sm="6" md="3">
+        <v-card
+          class="pa-4 cursor-pointer fill-height"
+          variant="outlined"
+          @click="dialog = true"
+          border
+          hover
+        >
+          <div class="d-flex align-center">
+            <v-icon icon="mdi-dots-horizontal" class="mr-2"></v-icon>
+            <div>
+              <div class="text-subtitle-1">查看全部</div>
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+<br/>
+    <v-dialog v-model="dialog">
       <v-card>
-        <v-card-title>选择作品类型</v-card-title>
+        <v-card-title>完整类型列表</v-card-title>
         <v-card-text>
           <v-container>
-            <v-card-item>
-              <v-card-title>特殊类型</v-card-title>
-              <v-card-subtitle
-                >使用 Scratch编辑器、纯文本展示器</v-card-subtitle
-              >
-            </v-card-item>
             <v-row>
               <v-col
-                v-for="(type, key) in specialTypes"
-                :key="key"
+                v-for="type in allTypes"
+                :key="`dialog-${type.key}`"
                 cols="12"
                 sm="6"
               >
                 <v-card
-                  :color="modelValue === key ? 'primary' : undefined"
+                  :color="modelValue === type.key ? 'primary' : undefined"
                   class="pa-4 cursor-pointer"
-                  :variant="modelValue === key ? 'tonal':undefined"
-                  @click="selectLanguage(key)"
+                  :variant="modelValue === type.key ? 'tonal' : undefined"
+                  @click="selectLanguage(type.key)"
                   border
                   hover
                 >
                   <div class="d-flex align-center">
-                    <v-icon :icon="type.icon" class="mr-2"></v-icon>
+                    <v-icon :icon="type.icon || 'mdi-shape-outline'" class="mr-2"></v-icon>
                     <div>
                       <div class="text-subtitle-1">{{ type.name }}</div>
-                      <div class="text-caption">{{ type.description }}</div>
-                    </div>
-                  </div>
-                </v-card>
-              </v-col>
-            </v-row>
-
-            <!-- 编程语言部分 --><v-card-item>
-              <v-card-title>编程语言</v-card-title>
-              <v-card-subtitle>使用 CodeRun 在线运行代码</v-card-subtitle>
-            </v-card-item>
-            <v-row>
-              <v-col
-                v-for="(lang, key) in languages"
-                :key="key"
-                cols="12"
-                sm="6"
-              >
-                <v-card
-                  :color="modelValue === key ? 'primary' : undefined"
-                  class="pa-4 cursor-pointer"
-                  :variant="modelValue === key ? 'tonal':undefined"
-                  @click="selectLanguage(key)"
-  border
-                  hover
-                >
-                  <div class="d-flex align-center">
-                    <v-icon :icon="lang.icon" class="mr-2"></v-icon>
-                    <div>
-                      <div class="text-subtitle-1">{{ lang.name }}</div>
-                      <div class="text-caption">{{ key }}</div>
+                      <div class="text-caption">{{ type.description || type.key }}</div>
                     </div>
                   </div>
                 </v-card>
@@ -84,7 +87,6 @@
 </template>
 
 <script>
-import languages from "@/constants/programming_languages.js";
 import specialTypes from "@/constants/special_languages.js";
 
 
@@ -107,11 +109,25 @@ export default {
   data() {
     return {
       dialog: false,
-      languages,
       specialTypes,
     };
   },
+  computed: {
+    allTypes() {
+      return Object.entries(this.specialTypes)
+        .filter(([key]) => !this.isHiddenType(key))
+        .map(([key, type]) => ({ key, ...type }));
+    },
+  },
   methods: {
+    isHiddenType(key) {
+      const lowered = String(key || "").toLowerCase();
+      return (
+        lowered === "#sym:programming_languages" ||
+        lowered.includes("coderun") ||
+        lowered.includes("codderun")
+      );
+    },
     selectLanguage(key) {
       this.$emit("update:modelValue", key);
       this.dialog = false;
